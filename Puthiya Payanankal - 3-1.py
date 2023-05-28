87a4b7fec8d31d9b46ee4caad945d9ba6c5aaa36
harithirumaran <134765919+harithirumaran@users.noreply.github.com>
2023-05-28 09:10:26 +0530

    Add files via upload

--- Puthiya Payanankal - 3-1.py
+++ Puthiya Payanankal - 3-1.py
@@ -9,9 +9,9 @@ def draw_circle(size,angle,shift):
  turtle.circle(size)
  turtle.right(angle)
  turtle.forward(shift)
- draw_circle(size+5,angle-5,shift-5)
+ draw_circle(size+5,angle+1,shift+1)
 
 turtle.bgcolor('black')
 turtle.speed('fast')
-turtle.pensize(5)
+turtle.pensize(40)
 draw_circle(30,0,1)
