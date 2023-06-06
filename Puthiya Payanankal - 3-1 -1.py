9f5378db8ff13ae8c181a8986236126b1ace76dc
harithirumaran <134765919+harithirumaran@users.noreply.github.com>
2023-06-06 11:55:05 +0530

    Add files via upload

--- Puthiya Payanankal - 3-1 -1.py
+++ Puthiya Payanankal - 3-1 -1.py
@@ -8,9 +8,9 @@ def draw_circle(size,angle,shift):
  turtle.circle(size)
  turtle.right(angle)
  turtle.forward(shift)
- draw_circle(size+5,angle+1,shift+1)
+ draw_circle(size+5,angle-20,shift-10)
 
 turtle.bgcolor('black')
 turtle.speed('fast')
-turtle.pensize(40)
+turtle.pensize(4)
 draw_circle(30,0,1)
--- Puthiya Payanankal - 3.py
+++ Puthiya Payanankal - 3.py
@@ -4,14 +4,13 @@ from itertools import cycle
 colors = cycle(['red','orange','yellow','green','blue','purple'])
 
 def draw_circle(size,angle,shift):
- turtle.bgcolor(next(colors))
  turtle.pencolor(next(colors))
  turtle.circle(size)
  turtle.right(angle)
  turtle.forward(shift)
- draw_circle(size+5,angle-5,shift-5)
+ draw_circle(size+5,angle+1,shift+1)
 
 turtle.bgcolor('black')
 turtle.speed('fast')
-turtle.pensize(5)
+turtle.pensize(4)
 draw_circle(30,0,1)
